756e1daad292ac25765182e126e282d3bce1e792
secnonsense <27748027+secnonsense@users.noreply.github.com>
2021-04-09 09:38:40 -0400

    Update ipsubcheck.py

--- ipsubcheck.py
+++ ipsubcheck.py
@@ -1,22 +1,47 @@
 from netaddr import IPNetwork, IPAddress
-import sys
+import argparse
 
-if len(sys.argv) < 2:
-    print("\nUsage: python3 ipsubnetcheck.py <file with list of IP's> <file with list of subnets to check>\n")
-    quit()
-else:
-    file=sys.argv[1]
-    file2=sys.argv[2]
+def compare_ip(ip,network,nomatch=0,f=0):
+    if IPAddress(ip) in IPNetwork(network):
+        print(f"IP {ip} matches {network}")
+        toggle=1
+        if f==1:
+            return toggle
+    elif nomatch==1 and f==0:
+        print(f"No match for {ip}")
 
-count=0
+def compare_files(file,file2,nomatch=0,f=1):
+    count=0
+    with open(file, 'r') as ip_file:
+            for my_ip in ip_file:
+                ip=str(my_ip.strip())
+                with open(file2, 'r') as network_file:
+                    match=0
+                    for my_network in network_file:
+                        network=str(my_network.strip())
+                        toggle=compare_ip(ip,network,nomatch,f)
+                        if toggle==1:
+                            match=1
+                            count+=1
+                    if match==0 and nomatch==1:
+                        print(f"No match for {ip}") 
+    print(f"\nTotal Matches: {count}\n")       
 
-with open(file, 'r') as ip_file:
-        for my_ip in ip_file:
-            ip=str(my_ip.strip())
-            with open(file2, 'r') as network_file:
-                for my_network in network_file:
-                    network=str(my_network.strip())
-                    if IPAddress(ip) in IPNetwork(network):
-                        print(f"IP {ip} matches network {network}")
-                        count+=1
-print(f"\nTotal matches is {count}\n")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help="Use files as input", action="store_true")
+    parser.add_argument("-n", "--nomatch", help="Show IP's that don't have a match", action="store_true")
+    parser.add_argument("source", help="Enter an IP to compare or a file with IP's with -f option")
+    parser.add_argument("dest", help="Enter a network to compare to, or a file of networks with the -f option")
+    args = parser.parse_args()
+
+    if args.file:
+        compare_files(args.source,args.dest,args.nomatch)
+    else:
+        compare_ip(args.source,args.dest,args.nomatch)
+
+
+if __name__ == "__main__":
+    main()
+
+
